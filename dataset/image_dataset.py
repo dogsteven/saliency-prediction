c7c931934ec1ff11ba65cc1698d434299fc4628a
khoahuynhbach <khoa.huynh_dogsteven@hcmut.edu.vn>
2022-11-10 21:31:45 +0700

    add generate

--- dataset/image_dataset.py
+++ dataset/image_dataset.py
@@ -1,8 +1,15 @@
 from torch.utils.data import Dataset
 from PIL import Image
 import os
+from typing import Tuple
 
 __all__ = ["ImageDataset", "ImageDirectoryDataset"]
+
+def parse(path: str) -> Tuple[str, str]:
+    exts = [".png", ".jpg", ".jpeg"]
+    for ext in exts:
+        if path.endswith(ext):
+            return path[0:-len(ext)], ext[1:]
 
 class ImageDataset(Dataset):
     def __init__(self, transform, mode: str = "RGB"):
@@ -20,10 +27,29 @@ class ImageDataset(Dataset):
 class ImageDirectoryDataset(ImageDataset):
     def __init__(self, directory, transform, mode: str = "RGB"):
         super().__init__(transform, mode)
-        self.directory = directory 
+        self.directory = directory
 
     def get_image_subpath(self, index: int) -> str:
         pass
 
     def get_image_path(self, index: int) -> str:
         return os.path.join(self.directory, self.get_image_subpath(index))
+
+    def generate(self, model, path: str, name: str, transform):
+        directory_path = os.path.join(path, name)
+        try:
+            os.mkdir(directory_path)
+        except:
+            print(f"Directory {name} already exsits")
+
+        for index in range(len(self)):
+            image = self[index].cuda().unsqueeze(0)
+            predicted = model(image).squeeze(0)
+            predicted = predicted[0].cpu().detach()
+            predicted = transform(predicted)
+
+            name, _ = parse(self.get_image_subpath(index))
+            name = f"{name}.png"
+            path = os.path.join(directory_path, name)
+
+            predicted.save(path, "PNG")
