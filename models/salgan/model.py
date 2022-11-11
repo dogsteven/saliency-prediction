235af38996acda12cd23cf53c23ee1de78be5791
khoahuynhbach <khoa.huynh_dogsteven@hcmut.edu.vn>
2022-11-12 03:01:42 +0700

    adjust learning rate

--- models/salgan/model.py
+++ models/salgan/model.py
@@ -5,7 +5,7 @@ __all__ = ["SalGAN"]
 
 class SalGAN(ConditionalGAN):
     def configure_optimizers(self):
-        g_optimizer = Adagrad(self.generator.parameters(), lr = 3e-10, weight_decay = 1e-4)
-        d_optimizer = Adagrad(self.discriminator.parameters(), lr = 3e-10, weight_decay = 1e-4)
+        g_optimizer = Adagrad(self.generator.parameters(), lr = 3e-4, weight_decay = 1e-4)
+        d_optimizer = Adagrad(self.discriminator.parameters(), lr = 3e-4, weight_decay = 1e-4)
 
         return [d_optimizer, g_optimizer], []
